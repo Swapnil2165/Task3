a4fed5c81dc578d7ecc9949dbc08a0705d7fb72f
swapnil2165 <swapnilchikanepatil2165@gmail.com>
2023-08-08 02:12:52 +0530

    message

--- src/Task3/Seaction.js
+++ src/Task3/Seaction.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { Accordion, AccordionTab } from 'primereact/accordion';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
-import "./Seaction.css"
+// import Button from 'react-bootstrap/Button';
+// import "./Seaction.css"
 
 const Container = () => {
     const [sections, setSections] = useState([]);
@@ -21,38 +22,46 @@ const Container = () => {
     };
 
     return (
-        <div className="container">
-            <div className="main">
-                <h2>Add Section</h2>
-                <div className="sesction">
-                    <label htmlFor="header">Section Header</label><br />
-                    <InputText
-                        id="header"
-                        value={header}
-                        onChange={(e) => setHeader(e.target.value)}
-                    />
-                </div>
-                <div className="sesction">
-                    <label htmlFor="details">Section Details</label><br />
-                    <InputText
-                        id="details"
-                        value={details}
-                        onChange={(e) => setDetails(e.target.value)}
-                    />
-                </div>
-                <Button label="Add" onClick={addSection} className="custom-button" />
-
-            </div>
-            <div className="wrapper">
-                <Accordion multiple activeIndex={[0]}>
+       <div className='container mt-5'>
+            <div className='card'>
+                <div className='card-body'>
+                     <div className='row'>
+                            <div className='col-sm-4 from-control border mx-auto'>
+                                <h2>Add Section</h2>
+                                <div className="">
+                                    <label htmlFor="header">Section Header</label><br />
+                                    <InputText className='form-control'
+                                        id="header"
+                                        value={header}
+                                        onChange={(e) => setHeader(e.target.value)}
+                                    />
+                                </div>
+                                <div className="sesction">
+                                    <label htmlFor="details">Section Details</label><br />
+                                    <textarea className='form-control'
+                                        id="details"
+                                        type='textarea'
+                                        value={details}
+                                        onChange={(e) => setDetails(e.target.value)}
+                                    />
+                                </div>
+                                <div className='mt-4 mb-4'>
+                                <Button variant="primary" label="Add"  onClick={addSection} className="custom-button form-control btn-primary" />
+                                </div>
+                        </div>
+                        <div className='col-sm-6 from-control border mx-auto'>
+                        <Accordion className='from-control'  multiple activeIndex={[0]}>
                     {sections.map((section, index) => (
-                        <AccordionTab key={index} header={section.header}>
+                        <AccordionTab className='from-control' key={index} header={section.header}>
                             <p>{section.details}</p>
                         </AccordionTab>
-                    ))}
-                </Accordion>
-            </div>
-        </div>
+                          ))}
+                         </Accordion>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+       </div>
     );
 };
 
